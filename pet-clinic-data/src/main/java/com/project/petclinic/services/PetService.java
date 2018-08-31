105400a7b90e1b5aa682d2f01426a12badc0c274
Michael Chambers <michael.chambers@nextgearcapital.com>
2018-08-30 22:58:15 -0400

    refactor services to use common interface. Closes #11

--- pet-clinic-data/src/main/java/com/project/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/project/petclinic/services/PetService.java
@@ -2,13 +2,8 @@ package com.project.petclinic.services;
 
 import com.project.petclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long>{
 
-    Pet findById(Long id);
 
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
